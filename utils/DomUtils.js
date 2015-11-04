4d41a871e692407889ba1bfcf3e95441264f59f5
Ray, Joel (TOR-MCL) <Joel.Ray@maclaren.com>
2015-11-04 16:37:58 -0500

    Added DOMUtils.js

---
+++ utils/DomUtils.js
@@ -0,0 +1,22 @@
+
+
+/**
+ * DOM Utils
+ *
+ * Note:
+ * The following is intended for snippet usage. 
+ * Ignore the async function when copying.
+ */
+(function() {
+
+	document.addEventListener('DOMContentLoaded', _handleDOMContentLoaded);
+
+
+	// ===================================================================
+	// EVENT INTERFACE ---------------------------------------------------
+
+	function _handleDOMContentLoaded(e) {
+		console.log('DOMUtils :: _handleDOMContentLoaded()');
+	}
+
+})();
